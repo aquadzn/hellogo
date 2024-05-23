c203d457ebe108716a5e2b73a6755a3ac5d594d6
aquadzn <williamjcqs8@gmail.com>
2024-05-23 08:52:57 +0200

    .

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	fmt.Printf("Server running (port=8080), route: http://localhost:8080/helloworld\n")
+
+	if err := http.ListenAndServe("0.0.0.0:8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
